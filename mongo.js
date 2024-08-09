6c30c3eda062d2d29bced40f50c2a30384ad4de6
Alicia <alicia_89_2@hotmail.com>
2024-08-09 11:23:11 +0200

    linter

--- mongo.js
+++ mongo.js
@@ -1,30 +1,25 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://alicia892:${password}@cluster0.nbbjtdl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  `mongodb+srv://alicia892:${password}@cluster0.nbbjtdl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
 
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-    content: 'Otra nota',
-    important: true,
-})
 
 /* note.save().then(result => {
     console.log('note saved!')
@@ -32,8 +27,8 @@ const note = new Note({
 }) */
 
 Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
